a064ba70079d6df4a8db40dca7782559bcf2adbf
mosio14 <mostafa.asadollahy@gmail.com>
2024-11-30 17:20:33 +0100

    add fontawsome

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {      
+      fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    colors: {
+      primary: '#84a98c', 
+      secondary: '#abc4b0', 
+    },
+  },
   },
   plugins: [],
 }

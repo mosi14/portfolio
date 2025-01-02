1cbb40a41a7b19b0e0f42e9c392cf0933c887bfa
mosio14 <mostafa.asadollahy@gmail.com>
2025-01-02 14:39:17 +0100

    add project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,155 +1,106 @@
+import { useState } from "react";
+import { projects } from "./listOfProjects";
+
 export default function Projects() {
+  const [expandedCard, setExpandedCard] = useState(false);
+
+  const toggleCard = (index) => {
+    setExpandedCard(expandedCard === index ? false : index);
+  };
+
+  const turncate = (str, length) => {
+    if (!str) return "";
+    return str.length > length ? str.slice(0, length) + "..." : str;
+  };
+
   return (
-    <div className="bg-gray-100 py-20 px-6 text-center">
+    <div className="bg-gray-100 py-20 px-6 ">
       <div className="inline-block  m-auto ">
         <h2 className="text-3xl font-bold ">Projects</h2>
         <div className="w-[90%] h-1 rounded bg-gradient-to-r from-info to-secondary m-auto"></div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
         {/* Add more project cards */}
-        <div className="p-6 bg-white rounded-lg shadow-md ">
-          <h2 className="text-2xl font-bold text-primary mb-4">
-            E-Commerce Web Application
-          </h2>
 
-          {/* Project Description with Magazine-Style Columns */}
-          <div className="text-gray-700 mb-4 gap-6 text-left">
-            <p>
-              A fully-featured e-commerce web application built using{" "}
-              <strong>React</strong>, <strong>Express</strong>, and{" "}
-              <strong>MongoDB</strong>. This project showcases a modern and
-              robust platform for online shopping with real-time features and an
-              intuitive interface.
+        {/* weather forecast project */}
+        {projects.map((project, index) => (
+          <div className="p-4 bg-white rounded-lg shadow-md ">
+            <div>
+              <img
+                className="shadow-sm rounded mb-4"
+                src={project.image}
+                alt={project.name}
+              />
+            </div>
+            <h2 className="text-2xl font-bold text-primary mb-4 text-center">
+              {project.name}
+            </h2>
+
+            {/* Features */}
+            {expandedCard === index && (
+              <>
+                {/* Description */}
+                {project.description !== "" && (
+                  <div>
+                    <p>{turncate(project.description, 165)}</p>
+                  </div>
+                )}
+                {project.features.length > 0 && (
+                  <>
+                    <h3 className="text-lg font-semibold text-secondary mb-2">
+                      Key Features:
+                    </h3>
+                    <ul className="list-disc  list-inside text-gray-700 mb-4">
+                      {project.features.map((feature, index) => (
+                        <li key={`feature_${index}`}>{feature}</li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+
+                {/* Technologies */}
+                {project.technologies.length > 0 && (
+                  <>
+                    <h3 className="text-lg font-semibold text-secondary mb-2">
+                      Technologies Used:
+                    </h3>
+                    <ul className="list-disc  list-inside text-gray-700 mb-4">
+                      {project.technologies.map((tec, index) => (
+                        <li key={`tec_${index}`}>{tec}</li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+              </>
+            )}
+            <p
+              className="text-secondary text-center text-sm hover:cursor-pointer"
+              onClick={() => toggleCard(index)}
+            >
+              {expandedCard === index ? "Show Less" : "Show More"}
             </p>
-            <p>
-              The platform includes a product catalog with advanced filtering
-              and search functionality, an admin panel for managing products and
-              orders, and a user-friendly checkout process. Additionally, it
-              offers secure payment options and automatic SMS notifications upon
-              purchase confirmation.
-            </p>
+
+            {/* Demo and Code */}
+            <div className="flex justify-between space-x-4">
+              <a
+                href={project.liveDemo}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-primary p-2 hover:bg-primary hover:text-white hover:rounded transition duration-300"
+              >
+                <i className="fas fa-link text-3xl"></i>
+              </a>
+              <a
+                href={project.viewCode}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-primary p-2 hover:bg-primary hover:text-white hover:rounded transition duration-300"
+              >
+                <i className="fab fa-github text-4xl"></i>
+              </a>
+            </div>
           </div>
-
-          {/* Features */}
-          <h3 className="text-lg font-semibold text-secondary mb-2">
-            Key Features:
-          </h3>
-          <ul className="list-disc list-inside text-gray-700 mb-4">
-            <li>
-              Product catalog with advanced filtering and search functionality.
-            </li>
-            <li>Admin panel to manage products, view and process orders.</li>
-            <li>Order tracking and user authentication system.</li>
-            <li>Automatic SMS notifications upon purchase confirmation.</li>
-            <li>
-              Secure payment gateway integration for seamless transactions.
-            </li>
-          </ul>
-
-          {/* Technologies */}
-          <h3 className="text-lg font-semibold text-secondary mb-2">
-            Technologies Used:
-          </h3>
-          <ul className="list-disc list-inside text-gray-700 mb-4">
-            <li>
-              <strong>Frontend:</strong> React, Tailwind CSS, Redux for state
-              management.
-            </li>
-            <li>
-              <strong>Backend:</strong> Express.js, Node.js, MongoDB for
-              database management.
-            </li>
-            <li>
-              <strong>Third-Party Services:</strong> SMS gateway integration
-              (e.g., Twilio).
-            </li>
-          </ul>
-
-          {/* Demo and Code */}
-          <div className="flex space-x-4">
-            <a
-              href="https://your-demo-link.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-primary text-white px-4 py-2 rounded hover:bg-secondary transition duration-300"
-            >
-              Live Demo
-            </a>
-            <a
-              href="https://github.com/your-repo-link"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="border border-primary text-primary px-4 py-2 rounded hover:bg-primary hover:text-white transition duration-300"
-            >
-              View Code
-            </a>
-          </div>
-        </div>
-
-        <div className="p-6 bg-white rounded-lg shadow-md ">
-          <h2 className="text-2xl font-bold text-primary mb-4">
-            Weather forecast Application
-          </h2>
-          <div className="border shadow-sm rounded mb-6 p-2">
-            <img src="/weather_app.png" alt="weather forecast app" />
-          </div>
-
-          {/* Features */}
-          <h3 className="text-lg font-semibold text-secondary mb-2">
-            Key Features:
-          </h3>
-          <ul className="list-disc text-left list-inside text-gray-700 mb-4">
-            <li>
-              <strong>Live Weather Data: </strong>Fetches and displays weather
-              data from the OpenWeatherMap API.
-            </li>
-            <li>
-              {" "}
-              <strong>Responsive Design:</strong> Fully responsive layout that
-              adapts to different screen sizes.
-            </li>
-            <li>
-              {" "}
-              <strong>Interactive UI: </strong>Users can search for cities and
-              view detailed weather information, including real-feel
-              temperature, humidity levels, and more.
-            </li>
-          </ul>
-
-          {/* Technologies */}
-          <h3 className="text-lg font-semibold text-secondary mb-2">
-            Technologies Used:
-          </h3>
-          <ul className="list-disc text-left list-inside text-gray-700 mb-4">
-            <li>
-              <strong>Frontend:</strong> React, Tailwind CSS
-            </li>
-            <li>
-              {" "}
-              <strong>APIs:</strong> OpenWeatherMap
-            </li>
-          </ul>
-
-          {/* Demo and Code */}
-          <div className="flex justify-between space-x-4">
-            <a
-              href="https://breezehub.vercel.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-primary text-white px-4 py-2 rounded hover:bg-secondary transition duration-300"
-            >
-              Live Demo
-            </a>
-            <a
-              href="https://github.com/mosi14/weather_app_design"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="border border-primary text-primary px-4 py-2 rounded hover:bg-primary hover:text-white transition duration-300"
-            >
-              View Code
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/components/listOfProjects.js
@@ -0,0 +1,53 @@
+export const projects = [
+    {
+      name: "E-Commerce Web Application",
+      image: "/ecommerce_app.png",
+      description:
+        "A fully-featured e-commerce web application built using React, Express, and MongoDB.",
+      features: [
+        "Product catalog with advanced filtering and search functionality.",
+        "Admin panel to manage products, view and process orders.",
+        "Order tracking and user authentication system.",
+        "Automatic SMS notifications upon purchase confirmation.",
+        "Secure payment gateway integration for seamless transactions.",
+      ],
+      technologies: [
+        "Frontend: React, Bootstrap, Redux for state management.",
+        "Backend: Express.js, Node.js, MongoDB for database management.",
+        "Third-Party Services: SMS gateway integration (e.g., Twilio).",
+      ],
+      liveDemo: "https://github.com/mosi14/OnlineStore",
+      viewCode: "https://github.com/mosi14/OnlineStore",
+    },
+    {
+        name: "Weather forecast Application",
+        image: "/weather_app.png",
+        description:"",
+    
+        features: [
+            "Live Weather Data: Fetches and displays weather data from the OpenWeatherMap API.",
+           " Responsive Design: Fully responsive layout that adapts to different screen sizes.",
+            "Interactive UI: Users can search for cities and view detailed weather information, including real-feel temperature, humidity levels, and more.",
+        ],
+        technologies: [
+          "Frontend: React, Tailwind CSS",
+          "APIs: OpenWeatherMap",
+        ],
+        liveDemo: "https://breezehub.vercel.app/",
+        viewCode: "https://github.com/mosi14/weather_app_design",
+      },
+    {
+      name: "MovieStream Application",
+      image: "/movie_streaming.png",
+      description:
+        "A movie streaming platform inspired by Netflix, where users can search for movies, explore genres, and get personalized recommendations.",
+      features: [],
+      technologies: [
+        "Frontend: React, Tailwind CSS",
+        "APIs: The Movie DB",
+      ],
+      liveDemo: "https://react-movie-verse.vercel.app/",
+      viewCode: "https://github.com/mosi14/react-MovieVerse",
+    },
+    // Add more projects as needed
+  ];
